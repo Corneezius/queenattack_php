6794943f664154e9ad3140a4447d923b9f9937b3
Cornelius Range and Ian Baum <cornelius@>
2016-09-12 16:36:44 -0400

    add rows laterally and vertically

--- src/queen.php
+++ src/queen.php
@@ -1,20 +1,36 @@
 <?php
     class Queen{
-
-        public $location;
-        public $xArray = ["A","B","C","D","E","F","G","H"];
-        public $yArray = [1,2,3,4,5,6,7,8];
-
         function canAttack($position, $target){
-          $position_array = str_split($position);
-          $target_array = str_split($target);
+            $x_array = ["a","b","c","d","e","f","g","h"];
+            $y_array = ["1","2","3","4","5","6","7","8"];
 
-          if($position_array[0] === $target_array[0]){
-            return true;
-          }
-          else{
-            return false;
-          }
+            $position_array = str_split($position);// = "d","4"
+            $target_array = str_split($target);// = "e","5"
+            $position_xAsNumber;
+            $index = 0;
+            foreach($x_array as $x){
+                if($x === $position_array[0]){
+                    $position_xAsNumber = $index;
+                    $index = 0;
+                }
+                $index++;
+            }
+
+            if($position_array[0] === $target_array[0] || $position_array[1] === $target_array[1]){
+                return true;
+            }
+            // else if($target_array[0] === $x_array[$position_xAsNumber +1] && $target_array[1] === $y_array[$position_array[1]]){
+            //     return true;
+            // }
+            else{
+                while($position_xAsNumber + $index < 8){
+                  if($target_array[0] === $x_array[$position_xAsNumber + $index + 1] && $target_array[1] === $y_array[$position_array[1] + $index]){
+                      return true;
+                  }else{
+                    $index++;
+                  }
+                }
+            }
 
         }
 
--- test/queenTest.php
+++ test/queenTest.php
@@ -6,8 +6,33 @@
         function test_QueenAttack_Row() {
           //Arrange
               $test_Queen = new Queen;
-              $position = "d6";
+              $position = "d4";
               $target = "d8";
+
+              //Act
+              $result = $test_Queen->canAttack($position, $target);
+
+              //Assert
+              $this->assertEquals(true, $result);
+        }
+
+        function test_QueenAttack_Column() {
+          //Arrange
+              $test_Queen = new Queen;
+              $position = "d4";
+              $target = "a4";
+
+              //Act
+              $result = $test_Queen->canAttack($position, $target);
+
+              //Assert
+              $this->assertEquals(true, $result);
+        }
+        function test_QueenAttack_UpRight() {
+          //Arrange
+              $test_Queen = new Queen;
+              $position = "d4";
+              $target = "e5";
 
               //Act
               $result = $test_Queen->canAttack($position, $target);
